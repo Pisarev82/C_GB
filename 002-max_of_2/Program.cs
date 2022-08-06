fe195c27006800c107a7d67294c0958a71cce62d
pisarev82 <pisarev82@yandex.ru>
2022-08-06 15:15:38 +0300

    Решение задачи 2. Нахождение максимума из двух чисел

--- 002-max_of_2/Program.cs
+++ 002-max_of_2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine()) ;
+Console.WriteLine("Введите еще одно целое число: ");
+int b = Convert.ToInt32(Console.ReadLine()) ;
+
+if(a > b) Console.WriteLine($"Число {a} больше чем число {b}. max = {a}");
+else Console.WriteLine($"Число {b} больше чем число {a}. max = {b}");
+
