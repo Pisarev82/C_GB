1c91dda058810b64fa874d7d5ae367d38f039703
pisarev82 <pisarev82@yandex.ru>
2022-08-07 11:59:35 +0300

    Задача 8. Добавленно решение при n меньше 1

--- 008_all_even/Program.cs
+++ 008_all_even/Program.cs
@@ -4,7 +4,21 @@
 // 8 -> 2, 4, 6, 8
 
 Console.Write("Введите целое число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-for (int count = 1; count <= n; count++)
-    if (count%2 == 0) Console.Write($"{count} ");
+int end = Convert.ToInt32(Console.ReadLine());
+int begin = 1;
+if (end == begin) Console.Write("N = 1, между 1 и 1 нет интервала для проверки");
+if (end>begin) 
+    {
+    while (begin <= end)
+        {
+            if (begin%2 == 0) Console.Write($"{begin} ");
+            begin++;
+        }
+    }
+else {
+    while (begin >= end)
+        {
+            if (begin%2 == 0) Console.Write($"{begin} ");
+            begin--;
+        }
+    }
