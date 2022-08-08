a9381482a1250264244b97f17ef2b3b344c7d495
Nikolay Pisarev <pisarev82@yandex.ru>
2022-08-08 20:45:40 +0300

    Работа на вебинаре. Максимальная цивра из двухзначного числа

---
+++ 009_/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая:
+// 1. выводит случайное число из отрезка [10, 99] и
+// 2. показывает наибольшую цифру числа.
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int number = new Random().Next(10,100); // 10, 99+1 (100 не входит в выдачу)
+//Console.WriteLine($"Случайное число из диапазона 10-99 => {number}");
+
+int firstDigit = number /10;
+int secondtDigit = number %10;
+
+// if (firstDigit == secondtDigit) Console.WriteLine($"цифры числа равны");
+// else if (firstDigit > secondtDigit) Console.WriteLine($"Наибольшая цифра {number} => {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра {number} => {secondtDigit}");
+
+//int result = firstDigit >= secondtDigit ? firstDigit : secondtDigit; // тернарный оператор
+
+
+string result = firstDigit == secondtDigit ? "Цыфры равны"
+: firstDigit > secondtDigit ? firstDigit.ToString() : secondtDigit.ToString();
+Console.WriteLine($"Наибольшая цифра {number} => {result}");
+
+
+
+
+
