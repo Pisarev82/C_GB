1a2d73796cdd8565d09affb190d19784853ead75
Nikolay Pisarev <pisarev82@yandex.ru>
2022-08-19 23:10:01 +0300

    домашнее задание к  семинару 5 от 18.08.2022

---
+++ 036/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] randomАrray = CreateArrayRndInt(6, 0, 10);
+PrintArray(randomАrray);
+Console.WriteLine($"Сумма элементов массива с нечетнымы индексами: {SumOddIndex(randomАrray)}");
+
+
+int[] CreateArrayRndInt(int size, int min, int max) 
+{
+    
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++) 
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+} 
+
+void PrintArray(int[] array) 
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int SumOddIndex (int[] АrrayForProcessing) 
+{
+    int sum = 0;
+    for (int i = 0; i < АrrayForProcessing.Length; i++)
+    {
+        if(i % 2 != 0) sum += АrrayForProcessing[i];
+    }
+    return sum;
+}
--- 038/Program.cs
+++ 038/Program.cs
@@ -1,10 +1,26 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
-int size = InputInt("Введите колличество элементов в массивеж ");
+int sizeArray = InputInt("Введите колличество элементов в массиве (целое положитльное число > 1): ");
+int maxVal = InputInt("Введите максимальное число в массиве (целое положитльное число > 1): ");
+int digitsAfterDot = InputInt("Введите колличество знаков после запятой (целое положительное число > 1): ");
+Console.WriteLine("Ошибки некорректного ввода будут обрабатываться в следующих версиях программы. Но это не точно.");
+if (sizeArray < 1 || maxVal < 1 || digitsAfterDot < 1)
+{
+    Console.WriteLine("Ошибки некорректного ввода будут обрабатываться в следующих версиях программы. Но это не точно.");
+}
+else Run();
 
+void Run()
+{
+    double[] array = CreateArrayRndInt(sizeArray, 0, maxVal, digitsAfterDot);
+    Console.WriteLine("Получился следующий массив");
+    PrintArray(array);
+    double[] resultForAnsver = ArrayDifferenceMaxMin(array);
+    Console.WriteLine($"В этом массиве максимальный элемент: {resultForAnsver[0]}, минимальный элемент: {resultForAnsver[1]}, а их разница: {resultForAnsver[2]}");
+}
 
-double [] CreateArrayRndInt(int size, int min, int max, int digitsAfterDot)  
+double [] CreateArrayRndInt(int size, int min, int max, int digitsAfterDotInner)  
 {
     
     double[] array = new double[size];
@@ -12,9 +28,8 @@ double [] CreateArrayRndInt(int size, int min, int max, int digitsAfterDot)
     double numberForArray = 0;
     for (int i = 0; i < size; i++) 
     {
-        int divider = Convert.ToInt32(Math.Pow(10, digitsAfterDot));
+        int divider = Convert.ToInt32(Math.Pow(10, digitsAfterDotInner));
         numberForArray = Convert.ToDouble(rnd.Next(min, max*divider))/ divider;
-        // numberForArray = Math.Round(numberForArray, digits, MidpointRounding.AwayFromZero);
         array[i] = numberForArray;
     }
     return array;
@@ -32,25 +47,23 @@ void PrintArray(double[] array)
     Console.WriteLine();
 }
 
-double [] array = CreateArrayRndInt(10, 0, 100, 1);
-PrintArray(array);
-
+// Имеются исключения представления некоторых чисел double в двоичной мантиссе, 
+// поэтому используем округление. Для более точного вычисления результата желательно привести все значения
+// массива в целое число и производить вычесления, но тогда теряется часть сути задачи.
 double[] ArrayDifferenceMaxMin (double [] arrayForActions) 
 {
     double[] result = new double[3];
     double min = arrayForActions[0];
-    double max = array[0];
-for (int i = 0; i < array.Length; i++)
-{
-    if (min > array[i]) min = array[i];
-    if (max < array[i]) max = array[i];
+    double max = arrayForActions[0];
+    for (int i = 0; i < arrayForActions.Length; i++)
+    {
+        if (min > arrayForActions[i]) min = arrayForActions[i];
+        if (max < arrayForActions[i]) max = arrayForActions[i];
+    }
+    double difference = Math.Round(max - min, digitsAfterDot, MidpointRounding.AwayFromZero);
+    result[0] = max; result[1] = min; result[2] = difference;
+    return result;
 }
-double result = max - min;
-result[0] = max; result[1] = max; result[2] = result;
-}
-
-System.Console.WriteLine($"{max} {min} {result}");
-
 
 int InputInt (string input)
 {
