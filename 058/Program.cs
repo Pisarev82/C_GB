6c1840a630ea67006b741c2a17d608bead9bbce3
pisarev82 <pisarev82@yandex.ru>
2022-09-02 14:07:49 +0300

    Решение задачи 58 задачи

--- 058/Program.cs
+++ 058/Program.cs
@@ -5,23 +5,19 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+// Так много комментариев, потому что долго разбирался с матрицими [2,3][3,2] оказывается их решением будет матрица [2,2]
 
-// int[,] CreateMatrixRndInt(int row, int col, int min, int max) 
-// {
-    
-//     int[,] matrix = new int[row, col];
-//     Random rnd = new Random();
+int[,] matrixFirst = 
+{
+    {2, 4 },
+    {3, 2 }
+};
 
-//     for (int i = 0; i < matrix.GetLength(0); i++) 
-//     {
-        
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             matrix[i,j] = rnd.Next(min, max + 1);
-//         }
-//     }
-//     return matrix;
-// } 
+int[,] matrixSecond = 
+{
+    {3, 4 },
+    {3, 3 }
+};
 
 void PrintMatrix(int[,] matrix) 
 {
@@ -34,65 +30,34 @@ void PrintMatrix(int[,] matrix)
             else Console.Write($"{matrix[i, j], 3} ");
         }
         Console.WriteLine("]");
-    }
-        
+    }      
 }
 
-int[,] matrix1 = 
+int[,] multiplicationMatrix (int[,] matrix1, int[,] matrix2)
 {
-    {2, 4, 5 },
-    {3, 2, 2 }
-};
-
-int[,] matrix2 = 
-{
-    {3, 4 },
-    {3, 3 },
-    {1, 4 }
-};
-
-int[] SelectionRow(int[,] matrix2, int row)
-{
-    int[] result = new int[matrix2.GetLength(0)];
-    for (int i = 0; i < matrix2.GetLength(0); i++)
+    // Размер результирующей матрицы ревен [колличество строк первой, колличество столбщов второй]
+    int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    // умножаются элементы строки первой матрицы на элементы столбщов второй матрицы
+    for (int i = 0; i < matrix1.GetLength(0); i++) // перебираем строки первой матрицы 
     {
-            result[i] = matrix2[row,i];
-    } 
-    return result;
-}
-int[] SelectionCol(int[,] matrix1, int col)
-{
-    int[] result = new int[matrix1.GetLength(0)];
-    for (int i = 0; i < matrix1.GetLength(1); i++)
-    {
-            result[i] = matrix1[i,col];
-    } 
-    return result;
-}
-
-int[,] productOfMatrices(int[,] matrix1, int[,] matrix2)
-{
-    int[,] result = new int[matrix1.GetLength(0),matrix2.GetLength(1)];
-    int[] row = SelectionRow(matrix2, 0);
-    int[] col = SelectionCol(matrix1, 0);
-    for (int i = 0; i < matrix2.GetLength(1); i++)
-    {
-        row = SelectionRow(matrix2, i);
-        for (int j = 0; j < matrix1.GetLength(0); j++)
+        for (int j = 0; j < matrix2.GetLength(1); j++) // перебираем столбцы второй матрицы 
         {
-            col = SelectionCol(matrix1, j);
-            for (int k = 0; k < row.Length; k++)
-            {  
-                result[i,j] += row[k]*col[k];
-            }
+            for (int k = 0; k < matrix1.GetLength(1); k++) // перебираем столбцы первой и строки второй (они равны, условие проверенно)
+            {
+                // номер строки первой матрицы = индекс строки результа матрицы
+                // номер столбца второй матрицы = индекс столбца результа матрицы
+                resultMatrix[i,j] += matrix1[i,k]*matrix2[k,j];
+            }          
         }
-    } 
-    return result;
+    }  
+    return resultMatrix;
 }
-// Если число столбцов первой матрицы равно числу строк второй матрицы их можно умножить.
-if (matrix1.GetLength(1) == matrix2.GetLength(0))
+
+
+// Первую матрицу можно умножить на вторую только 
+// если   столбцы(первой) == строки(второй)
+ if (matrixFirst.GetLength(1) == matrixSecond.GetLength(0))
 {
-int[,] multMatrix = productOfMatrices(matrix2,matrix1);
-PrintMatrix(multMatrix);
-Console.WriteLine($"{matrix1.GetLength(0)}, {matrix2.GetLength(1)}");
-} else Console.WriteLine("Умножать введенне матрицы нельзя");
+    PrintMatrix(multiplicationMatrix (matrixFirst, matrixSecond));
+} else Console.WriteLine("Матрицу 1 и 2 перемножить нельзя");
+
